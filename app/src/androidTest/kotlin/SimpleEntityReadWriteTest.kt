7e57296595eb35de0a440e2500d84add7cc65002
uHirsch <66682042+Swr96@users.noreply.github.com>
2022-04-29 16:05:27 +0200

    NEX-216_followTutorial implemented first working tests

--- app/src/androidTest/kotlin/SimpleEntityReadWriteTest.kt
+++ app/src/androidTest/kotlin/SimpleEntityReadWriteTest.kt
@@ -1,4 +1,5 @@
 import android.content.Context
+import androidx.lifecycle.asLiveData
 import androidx.room.Insert
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
@@ -6,6 +7,8 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.nexam.data.Exam
 import com.example.nexam.data.ExamDao
 import com.example.nexam.data.ExamRoomDatabase
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.runBlocking
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.IsEqual.equalTo
 import org.junit.*
@@ -33,17 +36,29 @@ class SimpleEntityReadWriteTest {
 
     @Test
     @Throws(Exception::class)
-    suspend fun writeUserAndReadInList() {
-        /*val exam: Exam = TestUtil.createUser(3).apply {
-            setName("george")
-        }
-        examDao.insert(user)
-        val byName = examDao.findUsersByName("george")
-        assertThat(byName.get(0), equalTo(user))*/
+    fun addAndReadExam() = runBlocking{
         val exam: Exam = Exam( 1, "Mathe", "2022-06-14")
         examDao.insert(exam)
         val byId = examDao.getExam(1)
-        assertThat(byId.toString(), equalTo(exam.toString()) )
-
+        assertThat(byId.firstOrNull(), equalTo(exam) )
+    }
+    @Test
+    @Throws(Exception::class)
+    fun updateExam() = runBlocking {
+        val exam: Exam = Exam( 1, "Mathe", "2022-06-14")
+        examDao.insert(exam)
+        val examUpdated: Exam = Exam(1, "Mathe", "2022-07-01")
+        examDao.update(examUpdated)
+        val byId = examDao.getExam(1)
+        assertThat(byId.firstOrNull(), equalTo(examUpdated) )
+    }
+    @Test
+    @Throws(Exception::class)
+    fun deleteExam() = runBlocking{
+        val exam: Exam = Exam( 1, "Mathe", "2022-06-14")
+        examDao.insert(exam)
+        examDao.delete(exam)
+        val byId = examDao.getExam(1)
+        assertThat(byId.firstOrNull(), equalTo(null))
     }
 }
