de87d56d4f8445f0ff1f4b004310ad00054cb33d
Franziska1402 <franziskaommer@gmail.com>
2022-05-29 14:34:51 +0200

    NEX-250 still fail

---
+++ app/src/androidTest/kotlin/com/example/nexam/features/ViewExamFeatureTest.kt
@@ -0,0 +1,45 @@
+package com.example.nexam.features
+
+import androidx.test.rule.ActivityTestRule
+import com.example.nexam.TestSuite
+import com.example.nexam.MainActivity
+import com.example.nexam.steps.DetailsSteps
+import com.example.nexam.steps.ScreenshotSteps
+import com.example.nexam.steps.ShowDashboardSteps
+import com.mauriciotogneri.greencoffee.GreenCoffeeConfig
+import com.mauriciotogneri.greencoffee.GreenCoffeeTest
+import com.mauriciotogneri.greencoffee.ScenarioConfig
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class ViewExamFeatureTest(scenarioConfig: ScenarioConfig?) : GreenCoffeeTest(scenarioConfig) {
+
+    @Rule
+    @JvmField
+    val activity = ActivityTestRule(MainActivity::class.java, true, true)
+
+    @Test
+    fun test() {
+        start(
+            ShowDashboardSteps(),
+            DetailsSteps(),
+            ScreenshotSteps()
+        )
+    }
+
+    companion object {
+        @Parameterized.Parameters(name = "{0}")
+
+        @JvmStatic
+        fun scenarios(): Iterable<ScenarioConfig> {
+            return GreenCoffeeConfig()
+                .withFeatureFromAssets("assets/viewExam.feature")
+                .takeScreenshotOnFail()
+                .scenarios(TestSuite.ENGLISH, TestSuite.SPANISH, TestSuite.GERMAN)
+        }
+    }
+}
+
---
+++ app/src/androidTest/kotlin/com/example/nexam/steps/ViewExamSteps.kt
@@ -0,0 +1,23 @@
+package com.example.nexam.steps
+
+import com.example.nexam.R
+import com.mauriciotogneri.greencoffee.GreenCoffeeSteps
+import com.mauriciotogneri.greencoffee.annotations.Then
+
+@Suppress("unused")
+class ViewExamSteps : GreenCoffeeSteps() {
+    @Then("^I see the detail screen for '([\\w| ]+)'$")
+    fun `iSeeTheDetailScreenFor$`(exam: String?) {
+        onViewWithText(exam).isDisplayed
+    }
+
+    @Then("^I see the subject is '([\\w| ]+)'$")
+    fun iSeeTheSubjectIs(name: String?) {
+        onViewWithId(R.id.exam_name).contains(name)
+    }
+
+    @Then("^I see the date is (\\d+)$")
+    fun iSeeTheDateIs(date: String?) {
+        onViewWithId(R.id.date).contains(date)
+    }
+}
